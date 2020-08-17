dfe28049d9093a83c5cf8f76520bfb6b49f2e2b9
Momojit Ghosh <momojit.ghosh@gmail.com>
2020-08-17 09:35:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import { HashRouter, Switch, Route} from 'react-router-dom';
 //import Navbar from './components/layout/Navbar';
 import WorldMap from './components/dashboard/WorldMap';
 import PlotGraph from './components/dashboard/PlotGraph';
@@ -56,7 +56,7 @@ class App extends Component {
           </div>
           <Topbar />
           <div className="App container">
-            <BrowserRouter>
+            <HashRouter>
               <Switch>
                 <Route exact path='/' component={WorldMap} />
                 <Route path='/world-stats' component={PlotGraph}  />
@@ -70,7 +70,7 @@ class App extends Component {
                 <Route path='/india-graphs' component={IndiaGraph} />
                 <Route path='/india-CovidTesting' component={Testing} />
               </Switch>
-            </BrowserRouter>
+            </HashRouter>
           </div>
           <div style={{ backgroundColor:"#333" , color:"white", width:"100%"}}>
               <h5 style={{textAlign:"right", padding:"2px"}}> &copy; 2020 mG.FightCovid-19 </h5>
